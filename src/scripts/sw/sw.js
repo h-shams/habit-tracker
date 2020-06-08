5f6614ff2f22923cfbdee9c3c4647c5787900ed4
Heydar Shams <shamsheidar.79@gmail.com>
2020-06-08 16:17:00 +0430

    add functions for comparison between new assets and old assets

--- src/scripts/sw/sw.js
+++ src/scripts/sw/sw.js
@@ -13,6 +13,59 @@ self.addEventListener('fetch', event => {
   console.log('SW: fetch for: ' + event.request.url)
 })
 
+function inCacheButAssets (assetsList, cacheList) {
+  return new Promise((resolve, reject) => {
+    if (!(assetsList && cacheList)) {
+      reject(new Error('two argunment must passed'))
+    }
+
+    const arr = []
+    cacheList.forEach(cacheEntry => {
+      let isFound = null
+      assetsList.forEach(assetsEntry => {
+        if (cacheEntry.url === assetsEntry.url) {
+        if (urlSlicer(cacheEntry.url) === assetsEntry.url) {
+          isFound = true
+        }
+      })
+      if (isFound === null) {
+        arr.push(cacheEntry.url)
+        arr.push(urlSlicer(cacheEntry.url))
+      }
+    })
+    resolve(arr)
+  })
+}
+
+function inAssetsButCache (assetsList, cacheList) {
+  return new Promise((resolve, reject) => {
+    if (!(assetsList && cacheList)) {
+      reject(new Error('two argunment must passed'))
+    }
+
+    const arr = []
+    assetsList.forEach(assetsEntry => {
+      let isFound = null
+      cacheList.forEach(cacheEntry => {
+        if (cacheEntry.url === assetsEntry.url) {
+          if (cacheEntry.revision === assetsEntry.revision) {
+            isFound = true
+          } else {
+            isFound = 'changed'
+          }
+        }
+      })
+      if (isFound === null) {
+        arr.push(assetsEntry.url)
+      } else if (isFound === 'changed') {
+        arr.push(assetsEntry.url)
+      } else {
+      }
+    })
+    resolve(arr)
+  })
+}
+
 function urlSlicer (url) {
   const baseUrlLength = ENV.baseUrl.length
   return url.slice(baseUrlLength)
